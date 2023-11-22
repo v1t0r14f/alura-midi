ecca578c5e5bf701cac51b99f727a20a245c67f4
v1t0r14f <148359817+v1t0r14f@users.noreply.github.com>
2023-11-22 11:06:47 -0300

    TRABALHO PRONTO
    1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
